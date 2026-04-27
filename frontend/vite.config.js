6dc55138b85ac150ae31e16e36af31c2c5cb867a
Amarnath WebDev <amarnathkumar2021@gmail.com>
2026-04-27 17:42:54 +0530

    update API proxy target to remote server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://139.59.66.31:5000/',
         changeOrigin: true,
         secure: false,
       },
